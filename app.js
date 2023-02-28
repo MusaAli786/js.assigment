2c6f8c2441a2318b6aef63cefa910e246fcb8b56
Musa Aly <120222011+MusaAli786@users.noreply.github.com>
2023-02-28 01:55:07 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,253 @@
+// chapter 1
+
+// question 1
+
+// var first="Error! please enter a valid password"
+// alert(first)
+// var second="Welcome to js land.... \n Happy coding!"
+// alert (second)
+// console.log="Hello.. i can run js through my web browsers console"
+// alert(console.log)
+
+// question 2
+
+// var world="Hello World"
+// alert (world)
+// var fullName="Musa Ali"
+// alert (fullName)
+// var age=25
+// alert (age)
+// var certifecate="web and mobile app development"
+// alert (certifecate)
+// var pizza="pizza \n pizz\n pizz\n piz \n pi"
+// alert (pizza)
+// var email="My email address is Alymusa145@gmail.com"
+// alert (email)
+// var book="i am trying to learn from the Book A smarter way to learn javascript"
+// alert (book)
+// var icon="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert (icon)
+
+// question 3
+
+// var age="24"
+// alert ("i am 24 year old")
+// var visite=14
+// alert ("you have visited this site 14 times")
+// var visitor= prompt(" what is your name?")
+// var product= prompt("product name?")
+// var quantity= prompt("how many product you want?")
+// alert("my visitor is"+" "+visitor+" "+ product+" "+quantity)
+
+
+// question 4
+
+// var hello="Hello World"
+// var name="my name is musa ali"
+// var age=24
+// legal variable
+// var firstName="musa"
+// var $last_name="ali"
+// var _age=24
+// var company$name="ali cloths"
+// var Nationality="pakistan"
+// ilegal variable
+// var firstname="musa"
+// var lastName="ali"
+// var agE="24"
+// var company.name="ali cloths"
+// var Nationality="pakistan"
+
+// question 5
+
+// var numOne=5
+// var numTwo=6
+// alert (numOne+numTwo)
+// alert (numOne=numTwo)
+// alert (numOne*numTwo)
+// alert (numOne/numTwo)
+// alert (numOne%numTwo)
+// var initial=5
+// alert (++initial)
+// alert (initial++)
+// alert (--initial)
+// var ticket_price="560"
+// var ticket_need="6"
+// alert (ticket_price*ticket_need)
+// var table=2
+// var table_2=prompt("enter any number")
+// alert (table*table_2)
+// console.log(table_2)
+// var f=10
+// var c=(f -32)*5/9
+// alert (c)
+// var farenhit=5
+// var f=(farenhit*9/5)+32
+// alert (f)
+// var price1= "price of item 1 is 650";
+// var quantity1= "quantity of item 1 is 3";
+// var price2= "price of item 2 is 100";
+// var quantity2= "quantity of item 2 is 7";
+// var shipping= "shipping charges 100";
+// document.write("<h1>Shopping Cart</h1>");
+// document.write(price1 + "<br>" + "<br>");
+// document.write(quantity1 + "<br>" + "<br>");    
+// document.write(price2 + "<br>" + "<br>");  
+// document.write(quantity2 + "<br>" + "<br>");  
+// document.write(shipping + "<br>" + "<br>"); 
+// var marks1= "Total marks 980"
+// var obtain1="Marks obtained 804"
+// docoment.write(marks1)
+// docoment.write(obtain1)
+
+
+// chapter 6
+
+// var a= "The value of a is: 10"
+// var a2="The value of ++a is :11"
+// var a3="Now the value of a is :11"
+// var a4= "The value of a++ is: 11"
+// var a5="Now the value of a is :12"
+// var a6="The value of --a is :11"
+// var a7="Now the value of a is :11"
+// var a8="The value of a-- is :11"
+// var a9="Now the value of a is :10"
+// document.write(a+"<br>" +"<br>" +"<br>" +"<br>")
+// document.write(a2  +"<br>")
+// document.write(a3 +"<br>" +"<br>"+"<br>")
+// document.write(a4 +"<br>")
+// document.write(a5 +"<br>"+"<br>")
+// document.write(a6 +"<br>")
+// document.write(a7 +"<br>"+"<br>")
+// document.write(a8 +"<br>")
+// document.write(a9)
+
+// var a = 2
+// var b = 1
+// var result = --a - --b + ++b + b--
+// document.write("a is 2" +"<br>")
+// document.write("b is 1" +"<br>")
+// document.write("result is 3" +"<br>")
+               
+
+//   chap 12 and 13
+
+// if and else statment
+// question No 1
+
+
+// let char = prompt("enter any character")
+// if(A = char && Z <= char){
+//     alert("your character is uppercase")
+// }
+// else if(a = char && z <=char){
+//     alert("your character is lowercase")
+// }
+// else{
+//     ("none of these")
+// }
+
+
+// question No 2
+
+
+// let sum = +prompt("enter any number")
+// let sum_2 = +prompt("enter any second number")
+// if(sum > sum_2){
+//     alert ("sum 1 is greter than sum 2")
+// }
+// else if(sum == sum_2){
+//     alert ("both number is equal")
+// }
+// else{
+//     alert ("not found result")
+// }
+
+
+// question No 3
+
+// let weather = +prompt ("enter weather tempreture")
+
+// if(weather <= 0){
+//     alert("tempteture is minus")
+// }else if(weather >0 ){
+//     alert("tempreture is plus")
+// }
+// else("not found")
+
+
+
+// question No 4
+
+
+// let vowel = prompt("enter any vowel")
+// if (vowel === "a" || (vowel === "e") || (vowel === "i") || (vowel === "o") || (vowel === "u")) {
+//     alert("true")
+// }
+// else {
+//     alert("false")
+// }
+
+// question No 5
+
+// let pasword = "aly1145"
+// let userPassword = prompt ("enter your password")
+// if(pasword == userPassword){
+//     alert("correct your password")
+// }
+// else{
+//     alert("your password is incorrect")
+// }
+
+
+// chapter 12 to 16
+
+// question 1 to 7
+
+// let students = [];
+// let colors = ["red", "blue", "green", "black", "yellow"];
+// let marks = [95, 65, 87, 57, 90];
+// let boolens = ["tru", "false"];
+// let mixed = ["musa", 25, "true"];
+// let qualification = ["SSC", "HSC", "BCS", "BS", "M.Phil", "PhD"];
+// document.write(qualification)
+
+// question 8
+
+// let studentsName = ["musa","waqar", "hasnain"];
+// let obtainMarks = [490, 410, 390];
+// let numberMusa  = 490/500*100;
+// let numberWaqar  = 410/500*100;
+// let numberHasnain  = 390/500*100; 
+
+// console.log(`score of ${studentsName[0]} is ${obtainMarks[0]} and his % is ${numberMusa}%`)
+// console.log(`score of ${studentsName[1]} is ${obtainMarks[1]} and his % is ${numberWaqar}%`)
+// console.log(`score of ${studentsName[2]} is ${obtainMarks[2]} and his % is ${numberHasnain}%`)
+
+
+// question no 9
+
+//  a
+
+// let color = ["green","blue","red","black"]
+// let inpute = prompt("which color you want add")
+// color.unshift(inpute);
+// console.log(color)
+
+
+// question 10
+
+
+// let studentScors = [320, 230, 480, 120]
+// studentScors.sort();
+// console.log(studentScors)
+
+// question 11
+
+// let citiesName = ["karachi", "lahore", "islamabad", "quetta", "peshawar"]
+// let b = citiesName.slice(0,2)
+// console.log(citiesName)
+
+
+
+
